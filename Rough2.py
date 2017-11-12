2dbc9d119fab0551d271f92e0e66571485ba465d
Shridhar Puntambekar <shridhar.puntambekar@gmail.com>
2017-11-11 18:00:58 -0800

    The final length

---
+++ Rough2.py
@@ -0,0 +1,87 @@
+import re
+
+regex = r"([\(\[]).*?([\)\]])"
+
+test_str = ("Saawariya-(MyMp3Singer.com.red.oo).mp3\n"
+            "Saawariya-(MyMp3Singercom).mp3\n"
+            "Samjhawan (Unplugged) - DownloadMing.SE\n"
+            "jannat mash-up by dj kiran kamath - kbps [www.djmaza.com].mp3khalbali(mympsong.com).mp3")
+ss = {1:'jannat mash-up by dj kiran kamath www.getlost.com - kbps [www.djmaza.com].mp3khalbali(mympsong.com)',2:'Samjhawan (Unplugged) www.somuchno.uk - DownloadMing.SE'}
+for ke,ite in ss.items():
+    matches = re.finditer(regex,ite, re.IGNORECASE | re.MULTILINE)
+    print(ite)
+    print('---------')
+    diff = 0
+    for matchNum, match in enumerate(matches):
+        matchNum = matchNum + 1
+
+        #print("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum=matchNum, start=match.start(),
+                                                         #                   end=match.end(), match=match.group()))
+
+        ite = ite.replace(ite[(match.start()-diff):(match.end()-diff)], '')
+        #print(ite)
+
+        diff = match.end() - match.start()
+        #print(diff)
+        print(ite)
+
+        ss[ke] = ite
+
+        '''for groupNum in range(0, len(match.groups())):
+            groupNum = groupNum + 1
+            print("Group {groupNum} found at {start}-{end}: {group}".format(groupNum=groupNum, start=match.start(groupNum),
+                                                                            end=match.end(groupNum),
+                                                                            group=match.group(groupNum)))'''
+
+regex2 = r"(\s?www.(.*?).com)|(\s?www.(.*?).pk)"
+
+for ke, ite in ss.items():
+    print('#################')
+    matches = re.finditer(regex2, ite, re.IGNORECASE | re.MULTILINE)
+    #print(ite)
+    #print('---------')
+    diff = 0
+    for matchNum, match in enumerate(matches):
+        matchNum = matchNum + 1
+
+        #print("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum=matchNum, start=match.start(),
+                                                                            #end=match.end(), match=match.group()))
+        print(ite)
+        ite = ite.replace(ite[(match.start() - diff):(match.end() - diff)], '')
+        print(ite)
+
+        diff = match.end() - match.start()
+        #print(diff)
+
+        ss[ke] = ite
+
+lkm = ["([\(\[]).*?([\)\]])","(\s?www.(.*?).com)|(\s?www.(.*?).pk)","(\s?([A-Z]*[a-z]*[0-9]*))\.com|((\s?([A-Z]*[a-z]*[0-9]*))\.pk)"]
+for thing in lkm:
+    print(r"{lk}".format(lk=thing))
+
+non_dup_set = ['1','2','3','4','5','6','7','7','8','8','0','1','2','4','3','2','5','6']
+dupl = {}
+i = 1
+for val in non_dup_set:
+    dupl[val] = 0
+
+print(dupl)
+
+i=0
+for val in non_dup_set:
+    if val in dupl:
+        i = i + 1
+        if i > len(dupl) and i not in dupl:
+            continue
+        else:
+            dupl[i] = dupl[i] + 1
+
+print(dupl)
+
+from collections import Counter
+ma = Counter(non_dup_set)
+print(ma)
+
+print(abs(1-5))
+
+lkms = {'aaaa':}
--- cleaningSongNames.py
+++ cleaningSongNames.py
@@ -2,9 +2,12 @@ import os
 import re
 import random
 import math
+from collections import Counter
 
-base_dir_r = r'C:\Users\shrip\OneDrive\Music\\'
+base_dir_r = r'C:\Users\shrip\Pictures\url_downloads\SwaTest\\'
 base_dir_w = r'C:\Users\shrip\Pictures\url_downloads\SwaTest\\'
+
+all_regexes = ["([\(\[]).*?([\)\]])","(\s?www.(.*?).com)|(\s?www.(.*?).pk)","(\s?([A-Z]*[a-z]*[0-9]*))\.com|((\s?([A-Z]*[a-z]*[0-9]*))\.pk)"]
 
 #Global map to store the dirty name as key and the clean name as value
 
@@ -41,15 +44,27 @@ def clean_names(song_names_dict):
 
     #Abstraction
     clean_spaces_and_number(only_name_index)
-    #For Brackets
-    pattern_matchng(only_name_index)
+
+                                            # For Brackets
+                                            #remov_bracketed_Data(only_name_index)
+    apply_all_filters(only_name_index)
+                                            #For unbracketed site names
+                                            #dele_site_ref(only_name_index)
+
+                                            #to clear songs.pk godo.pk without www
+                                            #clean_incomplete_sitenames(only_name_index)
 
     #For starting and ending trimming
     trim_data(only_name_index)
 
+    #To avoid collision for duplicate names
+    find_duplicates_in_name(only_name_index)
+
     #Write to notepad for better use
     writ_to_file(only_name_index)
 
+    #rename to disk
+    renam_the_files(only_name_index)
 
 def clean_spaces_and_number(song_names_dict):
     for k,v in song_names_dict.items():
@@ -57,38 +72,15 @@ def clean_spaces_and_number(song_names_dict):
         #Remove numbers
         v = v.lower().translate({ord(c): None for c in '0123456789'})
         #Replace '_' with spaces
-        v = v.translate({ord(c): 32 for c in '_%@-'})
+        v = v.translate({ord(c): 32 for c in '_%@-=$#&^*'})
         #Replace www and .com
         #replace multiple whitespaces with one space
-        z =' '.join((v).split())
+        z =' '.join(v.split())
         #Same as map.put()
         only_name_index[k] = z
         #print('--> ',z)
     return
 
-#How to write Reg Ex expression to remove everything written in [ ]
-#Only remove characters written in () if there is a '.' inside (.)
-def pattern_matchng(clean_name_map):
-    regex = r"([\(\[]).*?([\)\]])"
-    for ke, ite in clean_name_map.items():
-        matches = re.finditer(regex, ite, re.IGNORECASE | re.MULTILINE)
-        #print(ite)
-        #print('---------')
-        diff = 0
-        for matchNum, match in enumerate(matches):
-            matchNum = matchNum + 1
-
-            #print("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum=matchNum, start=match.start(),
-                                                                                #end=match.end(), match=match.group()))
-
-            ite = ite.replace(ite[(match.start() - diff):(match.end() - diff)], '')
-            #print(ite)
-
-            diff = match.end() - match.start()
-            #print(diff)
-
-        clean_name_map[ke] = ite
-    return
 
 #FOr testing purposes, write to a file
 def writ_to_file(song_names_dict):
@@ -109,10 +101,10 @@ def writ_to_file(song_names_dict):
     file_w.close()
 
 def trim_data(map_for_use):
-    regex = r"(\s?www.(.*?).com)|(\s?www.(.*?).pk)"
+
     rand_num = random.random()
     if rand_num < 1:
-        print('Cool')
+        #print('Cool')
         rand_num = math.floor(rand_num * 100000)
 
     for k,ite in map_for_use.items():
@@ -121,10 +113,74 @@ def trim_data(map_for_use):
             continue
 
         if ite[0] == ' ' or ite[0] == '\.':
-            print('cooler',k)
-            ite = ite[2:].lstrip()
+            #print('cooler',k)
+            ite = ite[2:].lstrip().rstrip()
 
-def pattern_match(regex,map_to_operate):
+def apply_all_filters(map_for_use):
+
+    for reg in all_regexes:
+        for ke, ite in map_for_use.items():
+
+            matches = re.finditer(reg, ite, re.IGNORECASE | re.MULTILINE)
+            # print(ite)
+            # print('---------')
+            diff = 0
+            for matchNum, match in enumerate(matches):
+                matchNum = matchNum + 1
+
+                # print("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum=matchNum, start=match.start(),
+                # end=match.end(), match=match.group()))
+
+                ite = ite.replace(ite[(match.start() - diff):(match.end() - diff)], '')
+
+
+                diff = match.end() - match.start()
+                # print(diff)
+
+                only_name_index[ke] = ite
+
+def find_duplicates_in_name(map_to_use):
+    '''non_dup_set = {}
+
+    for k in range(1,len(map_to_use)):
+        non_dup_set[k] = 0
+
+    print(non_dup_set)
+    print(len(map_to_use))
+
+    for i in range(1,len(map_to_use)):
+        for j in range (i,len(map_to_use)):
+            count = 1
+            if map_to_use[i] == map_to_use[j]:
+                print('--->',i)
+                print('--->',j)
+                non_dup_set[i] = non_dup_set[i] + 1
+                count = count + 1
+
+    print(non_dup_set)'''
+
+    dupl_count = Counter(map_to_use.values())
+
+    for k,v in map_to_use.items():
+        if dupl_count[v] is not 1:
+            only_name_index[k] = str(abs(1-dupl_count[v])) + '. ' + v
+            dupl_count[v] = dupl_count[v] - 1
+    '''for ke,val in map_to_use.items():
+        if val in non_dup_set and non_dup_set[val] > 0:
+           only_name_index[ke] = str(p) + val
+           non_dup_set[val] = non_dup_set[val] + 1
+           p = p + 1
+        else:
+           p=1'''
+
+    for k,v in only_name_index.items():
+       only_name_index[k] = only_name_index[k].title()
+
+def renam_the_files(map_to_use):
+    for ke,val in map_to_use.items():
+        
+
+
 
 read_songs(base_dir_r)
 
