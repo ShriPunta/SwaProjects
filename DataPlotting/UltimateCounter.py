821149d4455052132b998f50bca1acd4637b21ab
Shridhar Puntambekar <shridhar.puntambekar@gmail.com>
2018-04-14 14:41:27 -0700

    Better Anonymization Signed-off-by: Shridhar Puntambekar <shridhar.puntambekar@gmail.com>

--- DataPlotting/UltimateCounter.py
+++ DataPlotting/UltimateCounter.py
@@ -1,18 +1,42 @@
 class N_Counter:
     currentCount = 0
-    counterStorage = 'C:/Users/shrip/Documents/data/Anonymized/ultimateCounter.txt'
+    counterStorage = 'C:/Users/shrip/Documents/data/Anonymized/variables/ultimateCounter.txt'
 
     @staticmethod
     def getCurrentCount():
         first_line = ''
         f = open(N_Counter.counterStorage, 'r')
-        N_Counter.currentCount = int(f.readline())
+        try:
+            N_Counter.currentCount = int(f.readline())
+        except ValueError:
+            print("Counter File is empty")
+            f.close()
+            return
+        print("Reading 15",N_Counter.currentCount)
         f.close()
+        print("Reading 17",N_Counter.currentCount)
 
     @staticmethod
     def setNewCount():
         f = open(N_Counter.counterStorage,'w')
-        f.write(N_Counter.currentCount)
+        f.write(str(N_Counter.currentCount))
         f.close()
+
+    @staticmethod
+    def resetToOne():
+        first_line = ''
+        f = open(N_Counter.counterStorage, 'w+')
+        try:
+            cont = int(f.readline())
+        except ValueError:
+            print("Counter File is Empty")
+            return
+        if(cont != 1):
+            print("Resetting",N_Counter.currentCount)
+            N_Counter.currentCount=1
+            f.write('1')
+        f.close()
+
+
 
 
--- DataPlotting/deAnonymozing.py
+++ DataPlotting/deAnonymozing.py
@@ -1,39 +1,75 @@
+import time
 import os
 import pandas as pd
 import DataPlotting.UltimateCounter as coun
-base_dir = r'C:/Users/shrip/Documents/data/Anonymized'
+base_dir = ''
+write_dir = ''
 all_files_in_dir = []
 formatted_filenames = []
 globalCounta = coun.N_Counter
+error_file_names = []
+root_var_dir = r'C:/Users/shrip/Documents/data/Anonymized/variables/'
+
 
 def fill_list_with_fileNames(pathName):
     for root, dirs, files in os.walk(pathName, topdown=True):
         for filename in files:
-            if filename[-3:]!='csv':
+            if filename[-3:]==('csv') or filename[-4:]==('xlsx'):
+                all_files_in_dir.append(os.path.join(root, filename))
+            else:
                 continue
-            all_files_in_dir.append(os.path.join(root, filename))
+
+def fill_base_paths(variable_name,file_path):
+    global base_dir,write_dir
+
+    print(file_path)
+    print(variable_name)
+    if variable_name=='base_dir':
+        file_point=open(file_path,'r')
+        base_dir=str(file_point.readline()).strip()
+        file_point.close()
+    elif variable_name=='write_dir':
+        file_point = open(file_path, 'r')
+        write_dir = str(file_point.readline()).strip()
+        file_point.close()
 
 def start_anonymozing():
+    fill_base_paths('base_dir', root_var_dir+'base_dir.txt')
+    fill_base_paths('write_dir', root_var_dir+'dir_to_write.txt')
+    print('--->',base_dir)
+    print('===>',write_dir)
     fill_list_with_fileNames(base_dir)
+    print("Anon 22",globalCounta.currentCount)
+    globalCounta.resetToOne()
+    print("Anon 24",globalCounta.currentCount)
     for each_file_name in all_files_in_dir:
         openAndCopyEachFile(each_file_name)
+        break
+    print(error_file_names)
 
 def openAndCopyEachFile(fileName):
+    globalCounta.getCurrentCount()
+    print("Anon 32",globalCounta.currentCount)
     name,ext = os.path.splitext(os.path.basename(fileName))
-    getAttribs = ['ID','DOB','Country','Gender','AdmitType']
+    currYear = int(name[-4:])
     split_name = '_'.join(str(x) for x in name.split('_')[-2:])
-    split_name.join(ext)
-    print(split_name)
-
-    #dataf = pd.read_csv(fileName, skipinitialspace=True, usecols=getAttribs,nrows=500)
-    print(globalCounta.currentCount)
-    print(globalCounta.currentCount)
-    globalCounta.currentCount += 1
-    globalCounta.currentCount += 1
-    globalCounta.currentCount += 1
-    print(globalCounta.currentCount)
-    print(globalCounta.currentCount)
-
+    getAttribs = ['ID', 'DOB', 'Country', 'Career','Gender', 'AdmitType']
+    try:
+        dataf = pd.read_csv(fileName, skipinitialspace=True, usecols=getAttribs)
+        new_columns = dataf.columns.values;
+        new_columns[0] = 'UID';
+        dataf.columns = new_columns
+        for i,row in dataf.iterrows():
+            dataf.set_value(i, 'UID', globalCounta.currentCount)
+            #dataf.at[i,'UID'] = globalCounta.currentCount
+            globalCounta.currentCount+=1
+        dataf.to_csv(write_dir + split_name+ext, sep=',',index=False)
+        print(dataf.head())
+        globalCounta.setNewCount()
+    except UnicodeDecodeError:
+        error_file_names.append(split_name)
 
 if __name__ == '__main__':
+    start_time = time.time()
     start_anonymozing()
+    print("---%s seconds --"%(time.time() - start_time))
